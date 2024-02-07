489b2521f5515191acba2c592fe0147325edd28c
Lorrana Lacerda <146893895+lorranalacerda@users.noreply.github.com>
2024-02-06 22:00:03 -0300

    Add files via upload

---
+++ lista de tarefas/js/controle.js
@@ -0,0 +1,80 @@
+let contador = 0;
+let input = document.getElementById('inputtarefas');
+let btnAdd = document.getElementById('btn-add');
+let main = document.getElementById('arealista');
+
+function addtarefas() {
+    //PEGAR O VALOR DIGITADO NO INPUT
+    let valorInput = input.value;
+
+    // SE NAO FOR VAZIO, NEM NULO, NEM INDEFINIDO 
+    if((valorInput !== "") && (valorInput !== null) && (valorInput !==undefined)) {
+        
+        ++contador;
+    
+            let novoItem = `<div id="${contador}" class="item"> 
+                <div onclick="marcartarefa(${contador})" class="item-icone">
+                    < id="icone_${contador}" svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-circle" viewBox="0 0 16 16">
+                        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
+                    </svg> 
+
+                </div> 
+                <div class="item-nome">
+                    ${valorInput}
+                </div>
+                <div class="item-botao">
+                    <button onclick="deletar(${contador})" class="delete"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
+                        <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5"/>
+                    </svg>Deletar</button>
+                </div></div>`;
+
+        main.innerHTML += novoItem;
+
+        // ZERAR
+        input.value = "";
+        input.focus();
+    }
+}
+
+function deletar(id) {
+    var tarefa = document.getElementById(id);
+    tarefa.remove();
+}
+
+function marcartarefa(id) {
+    var item = document.getElementById(id);
+    var classe = item.getAttribute('class');
+    console.log(classe);
+
+    if (classe == "item"){
+        item.classList.add('clicado');
+
+        var icone = document.getElementById("icone_" + id);
+        icone.classList.remove(<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-circle" viewBox="0 0 16 16">
+        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
+    </svg>);
+        
+        icone.classList.add(<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-circle-fill" viewBox="0 0 16 16">
+        <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
+      </svg> );
+
+    item.parentNode.appendChild(item);
+    } else {
+        item.classList.remove('clicado');
+
+        var icone = document.getElementById('icone_' + id)
+        icone.classList.remove(<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-circle-fill" viewBox="0 0 16 16">
+        <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
+      </svg>);
+        icone.classList.add(<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-circle" viewBox="0 0 16 16">
+        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
+    </svg>);
+    }
+}
+
+input.addEventListener("keyup", function (event) {
+    if(event.keyCode === 13){
+       event.preventDefault();
+    btnAdd.click();
+    }
+});
